77503047895d1cd5e22d6a522aebc7851abe656f
Angelo Giuseppe De Michele <cerebro84@gmail.com>
2017-12-22 19:42:33 +0100

    Scheda 3

--- src/c/hello_world.c
+++ src/c/hello_world.c
@@ -9,7 +9,7 @@ static ScrollLayer *s_scroll_layer;
 static TextLayer *s_text_layer;
 
 // Lorum ipsum to have something to scroll
-static char s_scroll_text[] = "Riscaldamento, bpm 50% MFC :\r\n\r\n100 m crawl completo\r\n\r\n100 m dorso completo\r\n\r\n100 m rana completo\r\n\r\n \r\n\r\nA1 fase aerobica, bpm 55-60% MFC:\r\n\r\n2 x [8 x 25 m] crawl alternato con pausa di 2 sec. tra una bracciata e l\'altra e gambe alla massima frequenza possibile. Riposo 15 sec. tra uno scatto e l\'altro. Riposo di 1 min. tra le due serie\r\n\r\n\r\n \r\n\r\nA2 fase aerobica, bpm 65% MFC:\r\n\r\n4 x 50 m dorso completo. Riposo 15 sec. tra uno scatto e l\'altro\r\n\r\n20 x 25 m crawl completo con boccaglio. Riposo 10 sec. tra uno scatto e l\'altro\r\n\r\n \r\n\r\nDefaticamento:\r\n\r\n100 m a piacere";
+static char s_scroll_text[] = "Riscaldamento, bpm 50% MFC :\r\n\r\n200 m a piacere\r\nA2 fase aerobica, bpm 60% MFC:\r\n\r\n3 x 100 m, 50 m crawl completo + 50 m rana completa. Riposo 30 sec. tra uno scatto e l\'altro\r\n8 x 25 m, uno scatto delfino completo + uno scatto rana completa. Riposo 15 sec. tra uno scatto e l\'altro\r\n\r\nB1 soglia aerobica, bpm 75% MFC:\r\n8 x 25 m rana completa. Riposo 15 sec. tra uno scatto e l\'altro\r\n\r\n10 x 50 m braccia crawl con il galleggiante (pull buoy) tra le gambe. 25 m alla massima velocit\u00E0 e 25 m in scioltezza. Riposo 30 sec. tra uno scatto e l\'altro\r\n\r\nDefaticamento:\r\n200 m a piacere\r\n\r\nTot. 1600 m";
 
 // Setup the scroll layer on window load
 // We do this here in order to be able to get the max used text size
